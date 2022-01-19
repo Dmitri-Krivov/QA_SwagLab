5157fd65d5a28d93934b28e26fd23b1af59c204b
Dzmitry Kryvou <5625210@gmail.com>
2022-01-19 21:24:24 +0200

    Added PageFactory2

--- src/main/java/pageObject/AboutPage.java
+++ src/main/java/pageObject/AboutPage.java
@@ -2,17 +2,37 @@ package pageObject;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class AboutPage extends BasePage{
 
 //    Elements
-    By firstName = By.cssSelector("[id = 'firstname']");
-    By lastName = By.cssSelector("[id = 'lastname']");
-    By emailTextField = By.cssSelector("[name = 'email']");
-    By firstNameError = By.cssSelector("[id = 'firstname-error']");
-    By lastNameError = By.cssSelector("[id = 'lastname-error']");
-    By emailError = By.cssSelector("[id = 'email-error']");
-    By nextButton = By.cssSelector("#next");
+    @FindBy (css = "[id = 'firstname']")
+    WebElement firstName;
+    @FindBy (css = "[id = 'lastname']")
+    WebElement lastName;
+    @FindBy (css = "[id = 'email']")
+    WebElement emailTextField;
+    @FindBy (css = "[id = 'firstname-error']")
+    WebElement firstNameError;
+    @FindBy (css = "[id = 'lastname-error']")
+    WebElement lastNameError;
+    @FindBy (css = "[id = 'email-error']")
+    WebElement emailError;
+    @FindBy (css = "[id = 'next']")
+    WebElement nextButton;
+
+//    By firstName = By.cssSelector("[id = 'firstname']");
+//    By lastName = By.cssSelector("[id = 'lastname']");
+//    By emailTextField = By.cssSelector("[name = 'email']");
+//    By firstNameError = By.cssSelector("[id = 'firstname-error']");
+//    By lastNameError = By.cssSelector("[id = 'lastname-error']");
+//    By emailError = By.cssSelector("[id = 'email-error']");
+//    By nextButton = By.cssSelector("#next");
+
+
+
 
     public AboutPage(WebDriver driver) {
         super(driver);
@@ -20,7 +40,7 @@ public class AboutPage extends BasePage{
 
 //    Methods
     public void fillForm(String firstname, String lastname, String email){
-        fillText(driver.findElement(firstName), firstname);
+        fillText(firstName, firstname);
         fillLastName(lastname);
         fillEmail(email);
         clickNext();
@@ -28,31 +48,31 @@ public class AboutPage extends BasePage{
 
 
     public void fillFirstName(String text){
-        fillText(driver.findElement(firstName), text);
+        fillText(firstName, text);
     }
 
     public String getFirstNameError(){
-        return getElementText(driver.findElement(firstNameError));
+        return getElementText(firstNameError);
     }
 
     public void fillLastName(String text){
-        fillText(driver.findElement(lastName), text);
+        fillText(lastName, text);
     }
 
     public String getLastNameError(){
-        return getElementText(driver.findElement(lastNameError));
+        return getElementText(lastNameError);
     }
 
     public void fillEmail(String text){
-        fillText(driver.findElement(emailTextField), text);
+        fillText(emailTextField, text);
     }
 
     public String getEmailError(){
-        return getElementText(driver.findElement(emailError));
+        return getElementText(emailError);
     }
 
     public void clickNext(){
-        clickElement(driver.findElement(nextButton));
+        clickElement(nextButton);
     }
 
 }
